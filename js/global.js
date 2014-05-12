b82625e9c685c0f7547ab067e86246f803ce5dcf
Kay-P <KayPosmyk@gmx.de>
2014-05-12 22:16:13 +0200

    Bisherigen code kommentiert. Div aus index.html entfernt.

--- js/global.js
+++ js/global.js
@@ -1,4 +1,11 @@
 var controller;
+/***********************************************************************************
+ *    Grundsätzliches einrichten von Scroll Magic.
+ *    
+ *    Hinweis: ready ist ein jQuery Ereignis das aufgerufen wird, sobald der Browser
+ *             das erstellen des DOM-Baums beendet hat.
+ */
 $(document).ready(function($) {
+	// Erstellen des Scroll Magic controllers und horizontales Scrollen konfigurieren.
 	controller = new ScrollMagic({vertical: false});
 });
