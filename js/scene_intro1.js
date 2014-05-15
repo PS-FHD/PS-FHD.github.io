86b0657eb2f31731423289a7bfc188bca9db4e2d
Kay-P <KayPosmyk@gmx.de>
2014-05-15 21:33:33 +0200

    Zeichenkodierung UTF-8.

--- js/scene_intro1.js
+++ js/scene_intro1.js
@@ -6,10 +6,10 @@
  **********************************************************************************/
 $(document).ready(function($) {
 	/* Das Hintergrundbild im div wird um 500 pixel nach links verschoben.
-	   Dokumentation zu TweenMax und allen unterstützen Optionen: http://www.greensock.com/tweenmax/ */
+	   Dokumentation zu TweenMax und allen unterstÃ¼tzen Optionen: http://www.greensock.com/tweenmax/ */
 	var backgroundTween = TweenMax.to("#intro1", 1, {backgroundPosition: "-500px 0px", ease: Linear.easeNone});
 	/* Die Wolken fliegen jeweils von unterschiedlichen Startpositionen aus nach links zu einer jeweiligen Endposition. 
-	   Die unterschiedlichen Geschwindigkeiten entstehen dabei durch die unterschiedlichen Abstände zwischen Start- und Endposition. */
+	   Die unterschiedlichen Geschwindigkeiten entstehen dabei durch die unterschiedlichen AbstÃ¤nde zwischen Start- und Endposition. */
 	var cloudOneTween   = TweenMax.fromTo(".cloud.one", 1, {left: "50%"}, {left: "-15%", ease: Linear.easeNone});
 	var cloudTwoTween   = TweenMax.fromTo(".cloud.two", 1, {left: "30%"}, {left: "-25%", ease: Linear.easeNone});
 	var cloudThreeTween = TweenMax.fromTo(".cloud.three", 1, {left: "100%"}, {left: "10%", ease: Linear.easeNone});
@@ -28,8 +28,8 @@ $(document).ready(function($) {
 			airplane
 			]);
 	
-	/* Die Scroll Magic Scene für die erste Introszene definieren.
-	   Sie soll ab #intro1 1000px lang scrollen, wobei die position des Auslösers für diese Szene ganz an den Anfang gesetzt wird.*/
+	/* Die Scroll Magic Scene fÃ¼r die erste Introszene definieren.
+	   Sie soll ab #intro1 1000px lang scrollen, wobei die position des AuslÃ¶sers fÃ¼r diese Szene ganz an den Anfang gesetzt wird.*/
 	var scene = new ScrollScene({triggerElement: "#intro1", duration: 1000, triggerHook: 0})
 		.setTween(timelineTween)
 		.setPin("#intro1")
