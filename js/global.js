66450c7255815fd74d375ede4eca7cdc6d9f893b
Kay-P <KayPosmyk@gmx.de>
2014-05-17 10:15:23 +0200

    -Header und Footer sind nun bestandteil des Scrollcontainers, scrollen also mit. -Minimale und maximale Höhe festgelegt.

--- js/global.js
+++ js/global.js
@@ -8,4 +8,6 @@ var controller;
 $(document).ready(function($) {
 	// Erstellen des Scroll Magic controllers und horizontales Scrollen konfigurieren.
 	controller = new ScrollMagic({vertical: false});
+	
+	$("body > header").css("box-shadow", "0px 10px 10px 10px rgba(0, 0, 0, 0.3)");
 });
