bdd354e39097a76e9746a997e375e3bad8694408
Kay-P <KayPosmyk@gmx.de>
2014-05-25 13:55:39 +0200

    Codeformatierung, Klassennamen gefixt.

--- js/scene_intro1.js
+++ js/scene_intro1.js
@@ -17,33 +17,31 @@ $(document).ready(function($) {
 	var cloudFourTween  = TweenMax.fromTo("#intro1 > .cloud.four", 1, {left: "180%"}, {left: "25%", ease: Linear.easeNone});
 	
 	/* Das Flugzeug fliegt entgegen der Scrollrichtung, gewinnt dabei an Höhe und schrumpft bis es nicht mehr zu sehen ist
-	   Hierbei gibt scale den Vergrößerungsfaktor an. Bei scale:0 ist das Flugzeug nicht mehr sichtbar	*/
-	var airplane 	= TweenMax.fromTo(".airplane", 1, {left: "-25%", top: "150px",scale:2}, {left: "125%", top: "-15px", scale:0, ease: Linear.easeNone});	
+	   Hierbei gibt scale den Vergrößerungsfaktor an. Bei scale:0 ist das Flugzeug nicht mehr sichtbar. */
+	var airplane 	= TweenMax.fromTo("#intro1 > .airplane", 1, {left: "-25%", top: "150px", scale: 2}, {left: "125%", top: "-15px", scale: 0, ease: Linear.easeNone});
 	
-	/*headline kommt ONLOAD von oben und bounced mittels  ease: Bounce.easeOut
-	 * per scroll nach links aus dem bild*/
-	var headlineIn 	= TweenMax.fromTo("header.intro", 0.8, {top: "-30%",left:"2%"}, {top: "3%", ease: Bounce.easeOut});
-	var headlineOut = TweenMax.to("header.intro", 0.3, {left: "-50%", ease: Linear.easeNone});
+	/* headline kommt ONLOAD von oben und bounced mittels ease: Bounce.easeOut
+	   per scroll nach links aus dem bild. */
+	var headlineIn 	= TweenMax.fromTo("#intro1 > header.intro", 0.8, {top: "-30%",left:"2%"}, {top: "3%", ease: Bounce.easeOut});
+	var headlineOut = TweenMax.to("#intro1 > header.intro", 0.3, {left: "-50%", ease: Linear.easeNone});
 	
-	/* Der OMG Text wird mittels slala eingeblendet, dazu kommt eine rotation. Hierbei gibt rotation: die Drehung in Grad an*/
-	var textOmgIn 	= TweenMax.fromTo(".intro.bit.one", 0.5, {left: "7%", top: "37%", scale: 0}, {left: "7%", top: "37%", scale:1, rotation:342});
-	var textOmgOut 	= TweenMax.fromTo(".intro.bit.one", 0.3, {left: "7%", top: "37%"}, {left: "7%", top: "37%", scale:1, rotation:-180, scale: 0});	
-		
-	/* Der Text YES!! fliegt von links in das Bild beim Laden der Seite */
-	var textYESIn 	= TweenMax.to(".intro.bit.two", 0.5, { top:"40%", left:"30%", rotation:390, ease:Back.easeInOut});
-	var textYESOut 	= TweenMax.to(".intro.bit.two", 0.3, {top: "-80%", ease:Linear.easeNone});
-
-	/* Der Student-Status Text wird mittels easeIn eingbunden und dreht sich ein wenig */
-	var textStudentIN 	= TweenMax.to(".intro.bit.three", 1, {rotation:15, scale:3, ease:Back.easeInOut });
-	var textStudentOut 	= TweenMax.fromTo(".intro.bit.three", 0.3, {left: "25%", top: "60%"}, {left: "25%", top: "60%", rotation:-40, scale:0 });
+	/* Der OMG Text wird mittels slala eingeblendet, dazu kommt eine rotation. Hierbei gibt rotation: die Drehung in Grad an. */
+	var textOmgIn 	= TweenMax.fromTo("#intro1 > .bit.one", 0.5, {left: "7%", top: "37%", scale: 0}, {left: "7%", top: "37%", scale: 1, rotation: 342});
+	var textOmgOut 	= TweenMax.fromTo("#intro1 > .bit.one", 0.3, {left: "7%", top: "37%"}, {left: "7%", top: "37%", scale:1, rotation:-180, scale: 0});	
 	
-	var student = TweenMax.to(".student", 1, {left: "50%"});	
+	/* Der Text YES!! fliegt von links in das Bild beim Laden der Seite. */
+	var textYESIn 	= TweenMax.to("#intro1 > .bit.two", 0.5, {top: "40%", left: "30%", rotation: 390, ease:Back.easeInOut});
+	var textYESOut 	= TweenMax.to("#intro1 > .bit.two", 0.3, {top: "-80%", ease:Linear.easeNone});
 	
-	/* zweiter Screen */
+	/* Der Student-Status Text wird mittels easeIn eingbunden und dreht sich ein wenig. */
+	var textStudentIN 	= TweenMax.to("#intro1 > .bit.three", 1, {rotation:15, scale:3, ease:Back.easeInOut });
+	var textStudentOut 	= TweenMax.fromTo("#intro1 > .bit.three", 0.3, {left: "25%", top: "60%"}, {left: "25%", top: "60%", rotation: -40, scale: 0});
 	
-	var einstiegHeadIN 	= TweenMax.fromTo("header.einstieg", 0.65, {top: "-50%", left:"2%"}, { top:"17%", left:"2%",  ease: Linear.easeNone});
-
-	var einstiegTextIN 	= TweenMax.fromTo("section.einstieg", 0.65, {left: "2%", top: "100%"}, { left:"2%", top:"35%", ease: Linear.easeNone});
+	var student = TweenMax.to("#intro1 > .student", 1, {left: "50%"});	
+	
+	/* Zweiter Textblock. */
+	var einstiegHeadIN 	= TweenMax.fromTo("#intro1 > header.gettingstarted", 0.65, {top: "-50%", left:"2%"}, {top:"17%", left:"2%", ease: Linear.easeNone});
+	var einstiegTextIN 	= TweenMax.fromTo("#intro1 > section.gettingstarted", 0.65, {left: "2%", top: "100%"}, {left:"2%", top:"35%", ease: Linear.easeNone});
 	
 	var sceneChangeElement = $("#intro1 > .sceneChange");
 	// Die tatsächlich errechnete Breite der ersten Szene ermitteln.
@@ -54,7 +52,7 @@ $(document).ready(function($) {
 	var nextSceneIn = TweenMax.to("#intro2", 0.15, {top: "0", left: "0", startAt: {left: actualSceneWidth + "px"}, ease: Linear.easeNone});
 
 	
-	// Die Zeitleiste definieren.
+	// Die Zeitleiste mit allen Tweens definieren.
 	var timelineTween = new TimelineMax()
 		.add([
 			backgroundTween,
@@ -76,15 +74,16 @@ $(document).ready(function($) {
 		);
 	
 	/* Die Scroll Magic Scene für die erste Introszene definieren.
-	   Sie geht von 0px bis 1000px.*/
+	   Sie geht von 0px bis 1000px. */
 	var scene = new ScrollScene({duration: 1000, loglevel: 3})
 		.setTween(timelineTween)
 		.addTo(controller)
-		/*	update-Event wird von ScrollMagic beim Scrollen gefeuert. target liefert das DOM-Element, das das Event ausgeloest hat, also ScrollScene.
-			Parent von ScrollScene ist ScrollMagic.
-			info("scrollDirection") liefert FORWARD REVERSE oder PAUSED als String und gibt die ScrollRichtung an. 
-			info("scrollPos") liefert die aktuelle ScrollPosition als Ganzzahl. */
+		//  Update-Event wird von ScrollMagic beim Scrollen gefeuert.
 		.on("update", function (event) {
+			/* target liefert das DOM-Element, das das Event ausgeloest hat, also ScrollScene.
+			   Parent von ScrollScene ist ScrollMagic.
+			   info("scrollDirection") liefert FORWARD REVERSE oder PAUSED als String und gibt die ScrollRichtung an.
+			   info("scrollPos") liefert die aktuelle ScrollPosition als Ganzzahl. */
 			var scrollDirection = event.target.parent().info("scrollDirection");
 			var scrollPosition	= event.target.parent().info("scrollPos");
 			
