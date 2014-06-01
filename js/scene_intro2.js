8eda8ab6e6f963112f894008c71b2a8252523916
Kay-P <KayPosmyk@gmx.de>
2014-06-01 10:54:35 +0200

    -2. Szene: Bro-Fist und Studenten richtig ausgerichtet. -Szenenhöhe fix.

--- js/scene_intro2.js
+++ js/scene_intro2.js
@@ -9,9 +9,10 @@ $(document).ready(function($) {
 	// Das Hintergrundbild im div wird um 500 pixel nach links verschoben.
 	var backgroundTween = TweenMax.to("#intro2", 1, {backgroundPosition: "-500px 0px", ease: Linear.easeNone});
 	
-	var shineRotation = TweenMax.to("#intro2 > .shine", 1,  {rotation: 180, ease: Linear.easeNone}); 
-	var shineFadeIn   = TweenMax.fromTo("#intro2 > .shine", 0.2, {autoAlpha: 0}, {autoAlpha: 1, ease: Linear.easeNone});	
-	var shineFadeOut  = TweenMax.to("#intro2 > .shine", 0.2, {autoAlpha: 0, delay: 0.7, ease: Linear.easeNone});
+	var shineElement  = $("#intro2 > .students > .shine"); 
+	var shineRotation = TweenMax.to(shineElement, 1,  {rotation: 180, ease: Linear.easeNone}); 
+	var shineFadeIn   = TweenMax.fromTo(shineElement, 0.2, {autoAlpha: 0}, {autoAlpha: 1, ease: Linear.easeNone});	
+	var shineFadeOut  = TweenMax.to(shineElement, 0.2, {autoAlpha: 0, delay: 0.7, ease: Linear.easeNone});
 	
 	var textBlockHead = TweenMax.from("#intro2 > .textblock h2", 0.35, {top: "-40%", ease: Linear.easeNone});
 	var textBlockPara = TweenMax.from("#intro2 > .textblock p", 0.35, {top: "90%", ease: Linear.easeNone});
