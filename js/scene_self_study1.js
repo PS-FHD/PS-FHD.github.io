df4dd4915d1405c2a2a9e57606eec0de2ace9fde
Kay-P <KayPosmyk@gmx.de>
2014-06-08 11:37:19 +0200

    Verbesserungen Szene 4: -Verlängert. -Bessere Animationen. -Vordergrund jetzt prozentual. -Rechten tisch richtig benannt, ausgerichtet und skalliert. -Studenten richtig ausgerichtet (mittiger Protagonist zusammen mit Tisch nicht möglich).

--- js/scene_self_study1.js
+++ js/scene_self_study1.js
@@ -6,32 +6,26 @@
  *    Dokumentation zu TweenMax und allen unterstuetzen Optionen: http://www.greensock.com/tweenmax/
  **********************************************************************************/
 $(document).ready(function($) {
-	// Das Hintergrundbild im div wird um 840 pixel nach links verschoben.
+	// Das Hintergrundbild im div wird um 500 pixel nach links verschoben.
+	var background = TweenMax.to("#selfstudy1", 1, {backgroundPosition: "-500px 0px", ease: Linear.easeNone});	
 	
+	var textBlock = TweenMax.from("#selfstudy1 > .textblock", 0.125, {right: "-60%", ease: Linear.easeNone});
 	
-	var backgroundBooks = TweenMax.to("#selfstudy1", 1, {backgroundPosition: "-500px 0px", ease: Linear.easeNone});	
-	
-	var bookshelfBG =  TweenMax.fromTo("#selfstudy1 > .bookshelfBG", 0.5, {left: "120%"}, {left:"-70%", ease: Linear.easeNone});
-
-	var self1_textIn = TweenMax.fromTo("#selfstudy1 > .textblock", 0.4, {right: "-60%"}, {right: "2%", ease: Linear.easeNone});
-	
-	var self1_headOut = TweenMax.to("#selfstudy1 > header.self1", 0.1, {autoAlpha: 0, delay: 0.7});
-	var self1_textOut = TweenMax.to("#selfstudy1 > section.self1", 0.2, {autoAlpha: 0, delay: 0.7});
+	// Das Buecherregal im Vordergrund wischt ab 1/4 der Szene schnell von rechts nach links vorbei.
+	var bookshelfForeground = TweenMax.fromTo("#selfstudy1 > .foreground", 0.35, {left: "100%"}, {left: "-130%", delay: 0.35, ease: Linear.easeNone});
 
 	// Die Zeitleiste der Szene
 	var sceneTimeline = new TimelineMax()
 		.add([
-			backgroundBooks,
-			self1_textIn,
-			self1_headOut,
-			self1_textOut,
-			bookshelfBG
+			background,
+			textBlock,
+			bookshelfForeground
 		])
 		// Eigene Erweiterungsmethode um einen Szenenwechsel einzufuegen.
 		.addSceneChange($("#selfstudy1 > .sceneChange"), $("#selfstudy2"));
 	
 	// Die Scroll Magic Szene definieren und hinzufuegen. Sie wird in einem Scrollbereich von 7500px bis 10000px abgespielt.
-	addScene(new ScrollScene({duration: 2500})
+	addScene(new ScrollScene({duration: 5000})
 		.setTween(sceneTimeline)
 		.addTo(controller));
 });
