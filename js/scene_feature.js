944287da79460338a58fa08a94bbe9502250d6b4
dermeister1337 <unknown.member@gmx.de>
2014-06-13 21:20:57 +0200

    ergänzungen.

--- js/scene_feature.js
+++ js/scene_feature.js
@@ -9,26 +9,31 @@ $(document).ready(function($) {
 	// Das Hintergrundbild im div wird um 840 pixel nach links verschoben.
 	
 	
-	var boy1	= TweenMax.to("#feature > .boy.first", 0.62, {left:"25%", autoAlpha: 0, ease: Linear.easeNone, delay:0.07});
-	var boy2	= TweenMax.to("#feature > .boy.second", 0.62, {left:"25%", ease: Linear.easeNone, delay:0.07});
+	var boy1	= TweenMax.to("#feature > .boy.first", 0.5, {left:"25%", autoAlpha: 0, ease: Linear.easeNone, delay:0.5 });
+	var boy2	= TweenMax.to("#feature > .boy.second", 0.5, {left:"25%", ease: Linear.easeNone, delay:0.5});
 	
-	var logosIn	= TweenMax.fromTo("#feature > .logo.first, #feature > .logo.second, #feature > .logo.third, #feature > .logo.fourth, #feature > .logo.fifth, #feature > .logo.sixth, #feature > .logo.seventh,#feature > .logo.eighth,#feature > .logo.ninth", 0.1,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.1,ease: Linear.easeNone});
-	var logosOut= TweenMax.fromTo("#feature > .logo.first, #feature > .logo.second, #feature > .logo.third, #feature > .logo.fourth, #feature > .logo.fifth, #feature > .logo.sixth, #feature > .logo.seventh,#feature > .logo.eighth,#feature > .logo.ninth", 0.2,  {autoAlpha: 1} , {autoAlpha:0, delay: 0.3,ease: Linear.easeNone});
-	var logos2In	= TweenMax.fromTo("#feature > .logo2.first, #feature > .logo2.second, #feature > .logo2.third, #feature > .logo2.fourth, #feature > .logo2.fifth, #feature > .logo2.sixth, #feature > .logo2.seventh,#feature > .logo2.eighth", 0.1,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.6,ease: Linear.easeNone});
+	var logos1In	= TweenMax.fromTo("#feature > .logo.seventh, #feature > .logo.eighth" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.1,ease: Linear.easeNone});
+	var logos2In	= TweenMax.fromTo("#feature > .logo.third, #feature > .logo.sixth" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.2,ease: Linear.easeNone});
+	var logos3In	= TweenMax.fromTo("#feature > .logo.ninth, #feature > .logo.fifth" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.3,ease: Linear.easeNone});
+	var logos4In	= TweenMax.fromTo("#feature > .logo.fourth, #feature > .logo.first" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.4,ease: Linear.easeNone});
+	var logos5In	= TweenMax.fromTo("#feature > .logo.second" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.5,ease: Linear.easeNone});
 	
-	
-	var headIn 	= TweenMax.fromTo("#feature > .textblock", 0.1, {top: "15%", left:"-40%"}, { left:"15%", delay: 0.8, ease: Linear.easeNone});
+	var headIn 	= TweenMax.fromTo("#feature > .textblock", 0.1, {top: "5%", left:"160%"}, { left:"42%", delay: 0.75, ease: Linear.easeNone});
 
-	
-	
+	var logoFadeOut = TweenMax.to("#feature > .logo" , 0.05, {autoAlpha:0, delay: 0.75,ease: Linear.easeNone});
+	var logoBlock	= TweenMax.fromTo("#feature > .blackbox" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.75,ease: Linear.easeNone});
+
+	var logos6In	= TweenMax.fromTo("#feature > .logo2" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.75,ease: Linear.easeNone});
 	
 	// Die Zeitleiste der Szene
 	var sceneTimeline = new TimelineMax()
 		.add([
 
 		      boy1, boy2,
-		      logosIn, logosOut,
-		      logos2In,
+		       logos1In, logos2In, logos3In, logos4In, logos5In, 
+		       logoBlock,
+		       logoFadeOut,
+		       logos6In,
 		      headIn
 		    ]);
 		//Eigene Erweiterungsmethode um einen Szenenwechsel einzufuegen.
