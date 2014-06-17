4cc8b480d0eb875e707efd8644c002218fbfa557
Kay-P <KayPosmyk@gmx.de>
2014-06-17 17:38:10 +0200

    #102 Szene 6 verbessert: -Verlängertes Hintergrundbild übernommen. -Szene verlängert. -Bessere Animationssequenzen. -JS verbesserungen.

--- js/scene_self_study3.js
+++ js/scene_self_study3.js
@@ -6,55 +6,65 @@
  *    Dokumentation zu TweenMax und allen unterstuetzen Optionen: http://www.greensock.com/tweenmax/
  **********************************************************************************/
 $(document).ready(function($) {
-	var dayNight = TweenMax.to("#selfstudy3 > .middleground > .daynight", 1, {left: "-=31.55%", ease: Linear.easeNone});
+	var dayNight = TweenMax.to("#selfstudy3 > .middleground > .daynight", 1, {left: "-=100%", ease: Linear.easeNone});
 	
-	var table1Out = TweenMax.to("#selfstudy3 > .student.first", 0.1, {autoAlpha: 0, delay: 0.5, ease: Linear.easeNone});
-	var table2In  = TweenMax.fromTo("#selfstudy3 > .student.second", 0.1, {autoAlpha: 0}, {autoAlpha: 1, delay: 0.5, ease: Linear.easeNone});
-	var table2Out = TweenMax.to("#selfstudy3 > .student.second", 0.1, {autoAlpha: 0, delay: 0.7, ease: Linear.easeNone});
-	var table3In  = TweenMax.fromTo("#selfstudy3 > .student.third", 0.1, {autoAlpha: 0}, {autoAlpha: 1, delay: 0.7, ease: Linear.easeNone});
+	var table1Out = TweenMax.to("#selfstudy3 > .student.first", 0.1, {autoAlpha: 0, ease: Linear.easeNone});
+	var table2In  = TweenMax.fromTo("#selfstudy3 > .student.second", 0.1, {autoAlpha: 0}, {autoAlpha: 1, ease: Linear.easeNone});
+	var table2Out = TweenMax.to("#selfstudy3 > .student.second", 0.1, {autoAlpha: 0, ease: Linear.easeNone});
+	var table3In  = TweenMax.fromTo("#selfstudy3 > .student.third", 0.1, {autoAlpha: 0}, {autoAlpha: 1, ease: Linear.easeNone});
+	var doneBitIn = TweenMax.fromTo("#selfstudy3 > .bit", 0.1, {rotation: -45, autoAlpha: 0}, {autoAlpha: 1, ease: Linear.easeNone});
+	
+	var clock2 = TweenMax.fromTo("#selfstudy3 > .middleground > .clock.second", 0.1, {autoAlpha: 0}, {autoAlpha: 1, ease: Linear.easeNone});
+	var clock3 = TweenMax.fromTo("#selfstudy3 > .middleground > .clock.third", 0.1, {autoAlpha: 0}, {autoAlpha: 1, ease: Linear.easeNone});
+	var clock4 = TweenMax.fromTo("#selfstudy3 > .middleground > .clock.fourth", 0.1, {autoAlpha: 0}, {autoAlpha: 1, ease: Linear.easeNone});
+	var clock5 = TweenMax.fromTo("#selfstudy3 > .middleground > .clock.fifth", 0.1, {autoAlpha: 0}, {autoAlpha: 1, ease: Linear.easeNone});
+	
+	var textBlockOneIn = TweenMax.from("#selfstudy3 > .textblock:nth-of-type(1)", 0.05, {left: "-40%", ease: Linear.easeNone});
+	var textBlockOneOut = TweenMax.to("#selfstudy3 > .textblock:nth-of-type(1)", 0.05, {autoAlpha: 0});
+	var textBlockTwoIn = TweenMax.from("#selfstudy3 > .textblock:nth-of-type(2)", 0.05, {left: "-40%", ease: Linear.easeNone});
+	var textBlockTwoOut = TweenMax.to("#selfstudy3 > .textblock:nth-of-type(2)", 0.05, {autoAlpha: 0});
+	var textBlockThreeIn = TweenMax.from("#selfstudy3 > .textblock:nth-of-type(3)", 0.05, {left: "-40%", ease: Linear.easeNone});
+	
+	var tableTimeline = new TimelineMax()
+	  .appendMultiple([table1Out, table2In], 0.30)
+	  .appendMultiple([table2Out, table3In, doneBitIn], 0.20);
+	
+	var clockTimeline = new TimelineMax()
+	  .append(clock2, 0.15)
+	  .append(clock3, 0.15)
+	  .append(clock4, 0.15)
+	  .append(clock5, 0.15);
+	
+	// Da die Tweens fuer die Textbloecke hintereinander Abgespielt werden, ist hier eine zusaetzliche Zeitleiste sinnvoll.
+	var textBlockTimeline = new TimelineMax()
+		// Ab start der Zeitleiste den ersten Textblock ueber eine Dauer von 10% einblenden. 
+		.append(textBlockOneIn)
+		// .. danach 15% lang warten, dann den ersten Textblock ueber eine Dauer von 5% ausblenden.
+		.append(textBlockOneOut, 0.20)
+		// .. danach zweiten Textblock ueber eine Dauer von 10% einblenden etc.
+		.append(textBlockTwoIn)
+		.append(textBlockTwoOut, 0.15)
+		.append(textBlockThreeIn);
 	
 	var cups1 = TweenMax.fromTo("#selfstudy3 > .foreground.first", 1, {left: "-80%"}, {left: "500%", ease: Linear.easeNone});
 	var cups2 = TweenMax.fromTo("#selfstudy3 > .foreground.second", 1, {left: "-160%"}, {left: "400%", ease: Linear.easeNone});
 	var cups3 = TweenMax.fromTo("#selfstudy3 > .foreground.third", 1, {left: "-150%"}, {left: "300%", ease: Linear.easeNone});
 	var cups4 = TweenMax.fromTo("#selfstudy3 > .foreground.fourth", 1, {left: "-250%"}, {left: "200%", ease: Linear.easeNone});
 	
-	var clock2 = TweenMax.fromTo("#selfstudy3 > .middleground > .clock.second", 0.1, {autoAlpha:0}, {autoAlpha:1, delay:0.2, ease: Linear.easeNone});
-	var clock3 = TweenMax.fromTo("#selfstudy3 > .middleground > .clock.third", 0.1, {autoAlpha:0}, {autoAlpha:1, delay:0.35, ease: Linear.easeNone});
-	var clock4 = TweenMax.fromTo("#selfstudy3 > .middleground > .clock.fourth", 0.1, {autoAlpha:0}, {autoAlpha:1, delay:0.5, ease: Linear.easeNone});
-	var clock5 = TweenMax.fromTo("#selfstudy3 > .middleground > .clock.fifth", 0.1, {autoAlpha:0}, {autoAlpha:1, delay:0.65, ease: Linear.easeNone});
-	
-	var done = TweenMax.fromTo("#selfstudy3 > .bit", 0.1, {rotation: -45, autoAlpha:0}, {autoAlpha:1, delay: 0.7,  ease: Linear.easeNone});
-	
-	// Textbloecke vorselektieren
-	var firstTextblock  = $("#selfstudy3 > .textblock:nth-of-type(1)");
-	var secondTextblock = $("#selfstudy3 > .textblock:nth-of-type(2)");
-	var thirdTextblock  = $("#selfstudy3 > .textblock:nth-of-type(3)");
-	// Da die Tweens fuer die Textbloecke hintereinander Abgespielt werden, ist hier eine zusaetzliche Zeitleiste sinnvoll.
-	var textTimeline = new TimelineMax()
-		// Ab start der Zeitleiste den ersten Textblock ueber eine Dauer von 10% einblenden. 
-		.append(TweenMax.from(firstTextblock, 0.10, {left: "-40%", ease: Linear.easeNone}))
-		// .. danach 15% lang warten, dann den ersten Textblock ueber eine Dauer von 5% ausblenden.
-		.append(TweenMax.to(firstTextblock, 0.05, {delay: 0.15, autoAlpha: 0}))
-		// .. danach zweiten Textblock ueber eine Dauer von 10% einblenden etc.
-		.append(TweenMax.from(secondTextblock, 0.10, {left: "-40%", ease: Linear.easeNone}))
-		.append(TweenMax.to(secondTextblock, 0.05, {delay: 0.15, autoAlpha: 0}))
-		.append(TweenMax.from(thirdTextblock, 0.10, {left: "-40%", ease: Linear.easeNone}));
-	
 	// Die Zeitleiste der Szene
 	var sceneTimeline = new TimelineMax()
 		.add([
 			dayNight,
-			table1Out, table2In, table2Out,	table3In,
 			cups1, cups2, cups3, cups4,
-			textTimeline,
-			done,
-			clock2, clock3, clock4, clock5
+			tableTimeline,
+			textBlockTimeline,
+			clockTimeline
 		])
 		// Eigene Erweiterungsmethode um einen Szenenwechsel einzufuegen.
 		.addSceneChange($("#selfstudy3 > .sceneChange"), $("#canteen"));
 	
 	// Die Scroll Magic Szene definieren und hinzufuegen. Sie wird in einem Scrollbereich von 12500px bis 15000px abgespielt.
-	addScene("selfstudy3", new ScrollScene({duration: 2500})
+	addScene("selfstudy3", new ScrollScene({duration: 10000})
 		.setTween(sceneTimeline)
 		.addTo(controller));
 });
