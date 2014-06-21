a47790f12fd184bad4b9eed8548cdccdf94602ef
lol1337 <pantsu@meine-dateien.info>
2014-06-21 14:10:23 +0200

    änderung

--- js/scene_feature.js
+++ js/scene_feature.js
@@ -12,6 +12,10 @@ $(document).ready(function($) {
 	var boy1	= TweenMax.to("#feature > .boy.first", 0.5, {left:"25%", autoAlpha: 0, ease: Linear.easeNone, delay:0.5 });
 	var boy2	= TweenMax.to("#feature > .boy.second", 0.5, {left:"25%", ease: Linear.easeNone, delay:0.5});
 	
+//	var logos1In	= TweenMax.fromTo(".window-frame > .logo.column6,.window-frame >  .logo.column9" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.1,ease: Linear.easeNone});
+//	var logos2In	= TweenMax.fromTo(".window-frame > .logo.column5, .window-frame >  .logo.column8" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.2,ease: Linear.easeNone});
+//	var logos3In	= TweenMax.fromTo(".window-frame > .logo.column4, .window-frame > .logo.column7" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.3,ease: Linear.easeNone});
+//	var logos4In	= TweenMax.fromTo(".window-frame > .logo.column3, .window-frame > .logo.column1" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.4,ease: Linear.easeNone});
 	var logos1In	= TweenMax.fromTo(".window-frame > .logo.column2" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.1,ease: Linear.easeNone});
 	var logos2In	= TweenMax.fromTo(".window-frame > .logo.column7" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.2,ease: Linear.easeNone});
 	var logos3In	= TweenMax.fromTo(".window-frame > .logo.column8" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.3,ease: Linear.easeNone});
@@ -23,12 +27,12 @@ $(document).ready(function($) {
 	var logos9In	= TweenMax.fromTo(".window-frame > .logo.column4" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.9,ease: Linear.easeNone});
 	
 	
-	//var logos1In	= TweenMax.fromTo("#feature > .logo.column6, #feature > .logo.column9" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.1,ease: Linear.easeNone});
-	//var logos2In	= TweenMax.fromTo("#feature > .logo.column5, #feature > .logo.column8" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.2,ease: Linear.easeNone});
-	//var logos3In	= TweenMax.fromTo("#feature > .logo.column4, #feature > .logo.column7" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.3,ease: Linear.easeNone});
-	//var logos4In	= TweenMax.fromTo("#feature > .logo.column3, #feature > .logo.column1" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.4,ease: Linear.easeNone});
-	//var logos5In	= TweenMax.fromTo("#feature > .logo.column2" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.5,ease: Linear.easeNone});
-	
+//	var logos1In	= TweenMax.fromTo("#feature > .logo.column6, #feature > .logo.column9" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.1,ease: Linear.easeNone});
+//	var logos2In	= TweenMax.fromTo("#feature > .logo.column5, #feature > .logo.column8" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.2,ease: Linear.easeNone});
+//	var logos3In	= TweenMax.fromTo("#feature > .logo.column4, #feature > .logo.column7" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.3,ease: Linear.easeNone});
+//	var logos4In	= TweenMax.fromTo("#feature > .logo.column3, #feature > .logo.column1" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.4,ease: Linear.easeNone});
+//	var logos5In	= TweenMax.fromTo("#feature > .logo.column2" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.5,ease: Linear.easeNone});
+//	
 	
 //	var logos1In	= TweenMax.fromTo("#feature > .logo.seventh, #feature > .logo.eighth" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.1,ease: Linear.easeNone});
 //	var logos2In	= TweenMax.fromTo("#feature > .logo.third, #feature > .logo.sixth" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.2,ease: Linear.easeNone});
@@ -36,10 +40,10 @@ $(document).ready(function($) {
 //	var logos4In	= TweenMax.fromTo("#feature > .logo.fourth, #feature > .logo.first" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.4,ease: Linear.easeNone});
 //	var logos5In	= TweenMax.fromTo("#feature > .logo.second" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.5,ease: Linear.easeNone});
 
-	var headIn 	= TweenMax.fromTo("#feature > .textblock", 0.1, {top: "5%", left:"160%"}, { left:"42%", delay: 0.75, ease: Linear.easeNone});
+	var headIn 	= TweenMax.fromTo("#feature > .textblock", 0.1, {top: "5%", left:"160%"}, { left:"42%", delay: 1.0, ease: Linear.easeNone});
 
 	var logoFadeOut = TweenMax.to("#feature > .logo" , 0.05, {autoAlpha:0, delay: 0.75,ease: Linear.easeNone});
-	var logoBlock	= TweenMax.fromTo("#feature > .blackbox" , 0.05,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.75,ease: Linear.easeNone});
+	var logoBlock	= TweenMax.fromTo("#feature > .blackbox" , 0.1,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.75,ease: Linear.easeNone});
 
 	var logos10In	= TweenMax.fromTo("#feature > .logo2" , 0.1,  {autoAlpha: 0} , {autoAlpha:1, delay: 0.75,ease: Linear.easeNone});
 	
@@ -54,7 +58,7 @@ $(document).ready(function($) {
 		       logos10In,
 		      headIn
 		    ])
-	//Eigene Erweiterungsmethode um einen Szenenwechsel einzufuegen.
+		//Eigene Erweiterungsmethode um einen Szenenwechsel einzufuegen.
 		.addSceneChange($("#feature > .sceneChange"), $("#outro"));
 	
 	// Die Scroll Magic Szene definieren und hinzufuegen. Sie wird in einem Scrollbereich von 15000px bis 17500px abgespielt.
